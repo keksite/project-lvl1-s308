948540b6aa9ac7372219552b8b6cdf544509d383
keksite <keksite@mail.ru>
2018-07-10 01:32:25 +0300

    fourth lesson done

---
+++ src/bin/brain-even.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import { welcome, evenGame } from '..';
+
+evenGame(welcome());
--- src/index.js
+++ src/index.js
@@ -1,7 +1,39 @@
 import readlineSync from 'readline-sync';
 
-export default() => {
+export const welcome = () => {
   console.log('Welcome to the Brain Games! ');
+  console.log('Answer "yes" if number even otherwise answer "no".');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+  return name;
+};
+
+const isEven = number => number % 2 === 0;
+
+const isCorrectAnswer = (answer) => {
+  if (answer === 'yes') {
+    return true;
+  } if (answer === 'no') {
+    return false;
+  }
+  return undefined;
+};
+
+export const evenGame = (name) => {
+  let counter = 0;
+  while (counter < 3) {
+    const number = Math.floor(Math.random() * (100 - 0)) + 0;
+    console.log(`Question: ${number}`);
+    const answer = readlineSync.question('Answer is: ');
+    if (isEven(number) === isCorrectAnswer(answer)) {
+      console.log('Correct!');
+      counter++;
+    } else {
+      console.log(`'${answer}'' is wrong answer ;(. Correct answer was '${isEven(number) === true ? 'yes' : 'no'}'.Let's try again,${name}!`);
+      break;
+    }
+  }
+  if (counter === 3) {
+    console.log(`Congratulations, ${name}`);
+  }
 };
