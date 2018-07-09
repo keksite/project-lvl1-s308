2c0cef13748143168cec47f85d65afe755822d8e
keksite <keksite@mail.ru>
2018-07-09 20:05:15 +0300

    fix project.json , fix .gitignore(add .log), fix src/index.js, fix src/bin/brain-games.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import readlineSync from 'readline-sync';
 
-export default readlineSync;
+ export const welcome = () => {
+ 	console.log('Welcome to the Brain Games!');
+ 	const name = readlineSync.question('May I have your name? ');
+ 	console.log(`Hello, ${name}!`);
+ };
+
+
+
