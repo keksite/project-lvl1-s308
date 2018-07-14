78f6b5ecb57455618a38426eeda270dec7cacd19
keksite <keksite@mail.ru>
2018-07-14 14:44:03 +0300

    add prime game

---
+++ src/games/prime.js
@@ -0,0 +1,23 @@
+import getRandomInt from '../utils';
+import engine from '..';
+
+const minNum = 1;
+const maxNum = 100;
+const description = 'Is this number prime?';
+const questionString = 'Is this number prime?';
+
+const isPrime = (num) => {
+  if (num < 2) return false;
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
+const primeGame = () => {
+  const question = getRandomInt(minNum, maxNum);
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+  return { question, correctAnswer };
+};
+
+export default() => engine(description, primeGame, questionString);
